d03387fbbdc3dbb357321614d18ac882f63f4898
Hroi Sigurdsson <hroi@asdf.dk>
2020-06-01 11:31:46 +0000

    add object type constants

--- src/consts.rs
+++ src/consts.rs
@@ -1,3 +1,4 @@
+/// Paper sizes
 pub use pdflib_sys::a0_height as A0_HEIGHT;
 pub use pdflib_sys::a0_width as A0_WIDTH;
 pub use pdflib_sys::a1_height as A1_HEIGHT;
@@ -22,3 +23,14 @@ pub use pdflib_sys::letter_height as LETTER_HEIGHT;
 pub use pdflib_sys::letter_width as LETTER_WIDTH;
 pub use pdflib_sys::p11x17_height as P11X17_HEIGHT;
 pub use pdflib_sys::p11x17_width as P11X17_WIDTH;
+
+/// Object types
+pub use pdflib_sys::pcos_object_type_pcos_ot_array as PCOS_OT_ARRAY;
+pub use pdflib_sys::pcos_object_type_pcos_ot_boolean as PCOS_OT_BOOLEAN;
+pub use pdflib_sys::pcos_object_type_pcos_ot_dict as PCOS_OT_DICT;
+pub use pdflib_sys::pcos_object_type_pcos_ot_fstream as PCOS_OT_FSTREAM;
+pub use pdflib_sys::pcos_object_type_pcos_ot_name as PCOS_OT_NAME;
+pub use pdflib_sys::pcos_object_type_pcos_ot_null as PCOS_OT_NULL;
+pub use pdflib_sys::pcos_object_type_pcos_ot_number as PCOS_OT_NUMBER;
+pub use pdflib_sys::pcos_object_type_pcos_ot_stream as PCOS_OT_STREAM;
+pub use pdflib_sys::pcos_object_type_pcos_ot_string as PCOS_OT_STRING;
